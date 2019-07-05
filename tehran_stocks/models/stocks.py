dfef29a526d7fa1ba7b4114ee93e16ddf8ab4bb6
Mehdi Ghodsizadeh <mehdi.ghodsizadeh@gmail.com>
2019-07-05 16:19:09 +0430

    model: add stocks

---
+++ tehran_stocks/models/stocks.py
@@ -0,0 +1,48 @@
+from config.engine import *
+from sqlalchemy.orm import relationship
+import pandas as pd
+
+
+class Stocks(Base):
+    __tablename__ = "stocks"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    title = Column(String)
+    group_name = Column(String)
+    group_code = Column(Integer)
+    instId = Column(String)
+    insCode = Column(String)
+    code = Column(String, unique=True)
+    sectorPe = Column(Float)
+    shareCount = Column(Float)
+    estimatedEps = Column(Float)
+    baseVol = Column(Float)
+    prices = relationship("StockPrice", backref="stock")
+
+    def price_df(self):
+        query = f"select * from stock_price where code = {self.code}"
+        df = pd.read_sql(query, engine)
+        df["date"] = pd.to_datetime(df["dtyyyymmdd"], format="%Y%m%d")
+        self.df = df
+        return df
+
+
+class StockPrice(Base):
+    __tablename__ = "stock_price"
+
+    id = Column(Integer, primary_key=True)
+    code = Column(String, ForeignKey("stocks.code"))
+    ticker = Column(String)
+    date = Column("dtyyyymmdd", Integer)
+    first = Column(Float)
+    high = Column(Float)
+    low = Column(Float)
+    close = Column(Float)
+    value = Column(Integer)
+    vol = Column(Integer)
+    openint = Column(Integer)
+    per = Column(String)
+    open = Column(Float)
+    last = Column(Float)
+
