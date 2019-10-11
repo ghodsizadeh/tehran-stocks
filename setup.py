b4b763eaa32e3d895f4779424e9dd285a4bd10df
Gitea <gitea@fake.local>
2019-10-12 00:37:01 +0330

    Fix CLI for windows

--- setup.py
+++ setup.py
@@ -23,5 +23,5 @@ setup(
     install_requires=["pandas", "sqlalchemy"],
     zip_safe=False,
     python_requires=">=3.6",
-    scripts=["bin/ts-get"],
+    scripts=["bin/ts-get","bin/ts-get.bat"],
 )
