d7d4ba2ef5784eefcbc28af421cfd301b322dede
Mehdi Ghodsizadeh <mehdi.ghodsizadeh@gmail.com>
2020-04-05 18:56:00 +0430

    Updates

---
+++ precache-manifest.26c003da7acac50e9f8770094900cdd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc365ebacfb4cf805c3ddc45593025a7",
+    "url": "/tehran-stocks/index.html"
+  },
+  {
+    "revision": "459f8df777222fcc4b2a",
+    "url": "/tehran-stocks/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "80b1030a6ed765a0d52d",
+    "url": "/tehran-stocks/static/js/2.c7b5ad29.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tehran-stocks/static/js/2.c7b5ad29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "459f8df777222fcc4b2a",
+    "url": "/tehran-stocks/static/js/main.2a43e9ac.chunk.js"
+  },
+  {
+    "revision": "408fa62966761eaf6ff3",
+    "url": "/tehran-stocks/static/js/runtime-main.6fd4b4e9.js"
+  }
+]);
